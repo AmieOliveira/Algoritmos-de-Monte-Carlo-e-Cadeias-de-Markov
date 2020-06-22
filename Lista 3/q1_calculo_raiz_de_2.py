ec67723b0e3f0230dc00c64fb0c60f81b9c3b2e7
Amanda Oliveira <amandacnoliveira@poli.ufrj.br>
2020-06-22 11:25:01 -0300

    Codigos questoes 1 e 2 da lista 3

---
+++ Lista 3/q1_calculo_raiz_de_2.py
@@ -0,0 +1,47 @@
+"""
+    Autora: Amanda Oliveira (AmieOliveira)
+
+    Lista 3, questao 1
+    Implementacao do metodo de Monte Carlo para estimar o valor de sqrt(2)
+"""
+
+from random import random   # Utiliza o metodo Mersenne Twister
+from math import sqrt
+import matplotlib.pyplot as plt
+
+
+max_exp = 6
+estimate = []
+n_values = []
+
+r2 = sqrt(2)
+
+for exponent in range(max_exp+1):
+    n = 10**exponent     # Numero de amostras geradas (para o calculo da media amostral)
+
+    sum_g = 0
+
+    for idx in range(n):
+        # Ponto aleatorio dentro de um quadrado de lado 2
+        x = random()*2
+        y = random()*2
+        # Multiplicando por 2, passamos a ter um dominio [0,2)^2
+
+        g = 1 if (y <= 2 - x**2) else 0
+        sum_g += g
+
+    mean_g = sum_g/float(n)
+
+    r_2 = 3*mean_g   # Valor aproximado de sqrt(2)
+    #print(n, r_2, r2)
+
+    estimate += [ abs(r_2 - r2)/r2 ]
+    n_values += [ n ]
+
+plt.plot(n_values, estimate)
+plt.xscale('log')
+plt.title(r"Erro relativo do estimador de $\sqrt{2}$")
+plt.ylabel(r"$\frac{|\hat{e}_n - \sqrt{2}|}{\sqrt{2}}$")
+plt.xlabel("Número de amostras n")
+
+plt.show()
---
+++ Lista 3/q2_transformada_inversa.py
@@ -0,0 +1,41 @@
+"""
+    Autora: Amanda Oliveira (AmieOliveira)
+
+    Lista 3, questao 2
+    Geracao de amostras atraves do metodo da transformada inversa
+"""
+
+from random import random   # Utiliza o metodo Mersenne Twister
+from math import log
+import matplotlib.pyplot as plt
+
+n = 10000       # Numero de amostras a serem geradas
+n_bins = 50     # Numero de bins nos histogramas
+lam = 1
+alp = 10
+x_0 = 1
+
+exponencial = []
+pareto = []
+
+for sample in range(n):
+    u = random()
+    exponencial += [ -log(1 - u)/lam ]
+    pareto += [ x_0/((1 - u)**(1.0/alp)) ]
+
+
+#fig, axs = plt.subplots(,nrows=2, ncols=1)
+fig, axs = plt.subplots(2, 1,figsize=(5,7)) #, sharex=True)
+fig.suptitle("Função densidade de probabilidade empírica", y=.93)
+
+axs[0].hist(exponencial, bins=n_bins)
+axs[0].set_xlabel("Distribuição Exponencial")
+axs[0].text(.6, .7, r'$\lambda=1$', va='center', transform=axs[0].transAxes)
+
+
+axs[1].hist(pareto, bins=n_bins, color='gray')
+axs[1].set_xlabel("Distribuição de Pareto")
+axs[1].text(.6, .7, r'$\alpha=10$', va='top', transform=axs[1].transAxes)
+axs[1].text(.6, .7, r'$x_0=1$', va='bottom', transform=axs[1].transAxes)
+
+plt.show()
